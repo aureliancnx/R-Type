d1368b6fc7f688214815e4c3515b6dfdb3c79bdd
Chaika9 <bestdevelop22@gmail.com>
2022-09-21 17:48:48 +0200

    Add cmake flag for is Server or Client

---
+++ ClientMain.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int ac, char **av) {
+    std::cout << "Starting R-Type Client..." << std::endl;
+    return 0;
+}
---
+++ ServerMain.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int ac, char **av) {
+    std::cout << "Starting R-Type Server..." << std::endl;
+    return 0;
+}

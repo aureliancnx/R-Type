e6473f04e457b6611ac28fa3bb45287dd09d513c
Chaika9 <bestdevelop22@gmail.com>
2022-09-22 18:27:25 +0200

    Test build

--- ClientMain.cpp
+++ ClientMain.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 
-#include "KapEngine.hpp"
-#include "Graphical/RaylibGraphical.hpp"
+// #include "KapEngine.hpp"
+// #include "Graphical/RaylibGraphical.hpp"
+
+// int main(int ac, char **av) {
+//     // std::cout << "Starting R-Type Client..." << std::endl;
+
+//     KapEngine::KapEngine engine(true, "RType", "Alpha", "Epitech tkt on gère");
+
+//     auto raylib = std::make_shared<KapEngine::Graphical::RaylibGraphical>(*engine.getGraphicalLibManager());
+
+//     engine.getGraphicalLibManager()->addLib(raylib);
+//     engine.getGraphicalLibManager()->changeLib("raylib");
+
+//     engine.run();
+
+//     return 0;
+// }
 
 int main(int ac, char **av) {
-    // std::cout << "Starting R-Type Client..." << std::endl;
-
-    KapEngine::KapEngine engine(true, "RType", "Alpha", "Epitech tkt on gère");
-
-    auto raylib = std::make_shared<KapEngine::Graphical::RaylibGraphical>(*engine.getGraphicalLibManager());
-
-    engine.getGraphicalLibManager()->addLib(raylib);
-    engine.getGraphicalLibManager()->changeLib("raylib");
-
-    engine.run();
-
+    std::cout << "Starting R-Type Client..." << std::endl;
     return 0;
 }

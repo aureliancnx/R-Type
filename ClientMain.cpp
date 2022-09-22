f9a49aae79b1705bb41a534864fa31178c158944
benji-35 <benjamin.delvert@epitech.eu>
2022-09-22 17:30:21 +0200

    CHG : add engine running on client

--- ClientMain.cpp
+++ ClientMain.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+#include "KapEngine.hpp"
+#include "Graphical/RaylibGraphical.hpp"
+
 int main(int ac, char **av) {
-    std::cout << "Starting R-Type Client..." << std::endl;
+    // std::cout << "Starting R-Type Client..." << std::endl;
+
+    KapEngine::KapEngine engine(true, "RType", "Alpha", "Epitech tkt on gère");
+
+    auto raylib = std::make_shared<KapEngine::Graphical::RaylibGraphical>(*engine.getGraphicalLibManager());
+
+    engine.getGraphicalLibManager()->addLib(raylib);
+    engine.getGraphicalLibManager()->changeLib("raylib");
+
+    engine.run();
+
     return 0;
 }

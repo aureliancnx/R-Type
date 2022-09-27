9e6a8cb5e11a2e139ac8b934e8700fa6a5b638f0
Chaika9 <bestdevelop22@gmail.com>
2022-09-27 19:08:56 +0200

    Move KapMirror to Packages

--- Library/library.cmake
+++ Library/library.cmake
@@ -2,8 +2,3 @@
 add_subdirectory(Library/KapEngine)
 target_link_libraries(RType KapEngine)
 include(Library/KapEngine/CMake/engine_include.cmake)
-
-# KapMirror
-add_subdirectory(Library/KapMirror)
-target_link_libraries(RType KapMirror)
-include_directories(Library/KapMirror)
--- Packages/packages.cmake
+++ Packages/packages.cmake
@@ -1,2 +1,7 @@
 include_directories(.)
+
+# KapRaylib
 include_directories(Packages/KapRaylib)
+
+# KapMirror
+include_directories(Packages/KapMirror)

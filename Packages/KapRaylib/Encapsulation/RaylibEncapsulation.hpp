a0babf005edc854f927c52bcef8df75cefe6f1e6
benji-35 <benjamin.delvert@epitech.eu>
2022-09-29 15:12:22 +0200

    FIX : texure drawing method

--- Packages/KapRaylib/Encapsulation/RaylibEncapsulation.hpp
+++ Packages/KapRaylib/Encapsulation/RaylibEncapsulation.hpp
@@ -194,6 +194,7 @@ namespace KapEngine {
                             ImageFlipHorizontal(img);
                             size.y *= -1.f;
                         }
+                        ImageResize(img, size.x, size.y);
                     }
 
                     /**

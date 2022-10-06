84e3a6a7676959b8d684ebcab2284d695c69f9e1
Dosser <paul.dosser@epitech.eu>
2022-10-06 11:33:25 +0200

    ADD: change class to add direction of the shoot in the player class

--- Assets/Components/Player/Shoot/Shoot.cpp
+++ Assets/Components/Player/Shoot/Shoot.cpp
@@ -11,35 +11,50 @@ namespace KapEngine {
             init(gameObject);
         }
 
+        Shoot::Shoot(std::shared_ptr<GameObject> gameObject, const std::string &path, const std::string &sound) : Component(gameObject, "Shoot") {
+            _path = path;
+            _sound = sound;
+            init(gameObject);
+        }
+
         void Shoot::init(std::shared_ptr<GameObject> gameObject) {
             addRequireComponent("Image");
+            playSound(_sound);
 
             auto img = std::make_shared<UI::Image>(gameObject);
-            img->setPathSprite("Assets/Textures/shoot.png");
-            img->setRectangle({0.f, 0.f, 19, 6});
+            if (!_path.size() == 0)
+                img->setPathSprite(_path);
+            else
+                img->setPathSprite("Assets/Textures/shoot.png");
+
+                img->setRectangle({0.f, 0.f, 19, 6});
 
             gameObject->addComponent(img);
 
             try {
                 Transform &tr = (Transform &)gameObject->getTransform();
-                tr.setScale({(19 * 4), (6 * 4)});
+                tr.setScale({(19 * 2), (6 * 2)});
             } catch (...) {}
         }
 
         void Shoot::onFixedUpdate() {
 
-            Tools::Vector3 cPos = getTransform().getLocalPosition();
+            _pos.setX((_pos.getX() + _direction.getX()));
+            _pos.setY((_pos.getY() + _direction.getY()));
 
-            _pos.setX((_pos.getX() + 0.5f));
+            _cPos += _pos;
 
-            cPos += _pos;
+            getTransform().setPosition(_cPos);
 
-            getTransform().setPosition(cPos);
-
-            if (cPos.getX() >= getGameObject().getEngine().getScreenSize().getX()) {
+            Tools::Vector2 screenSize = getGameObject().getEngine().getScreenSize();
+            if (_cPos.getX() >= screenSize.getX() || _cPos.getY() >= screenSize.getY()) {
                 getGameObject().destroy();
                 return;
             }
+        }
+
+        void Shoot::setDirection(Tools::Vector2 direction) {
+            _direction = direction;
         }
 
         void Shoot::animation() {
@@ -61,6 +76,34 @@ namespace KapEngine {
             animator->addAnim(shootAnimation, "Shoot");
 
             animator->addLink("Shoot", "Shoot", "To shoot");
+        }
+
+        void Shoot::setPos(Tools::Vector3 pos) {
+            _cPos = pos;
+        }
+
+        void Shoot::setPath(std::string const &path) {
+            getImage().setPathSprite(path);
+        }
+
+        void Shoot::setSound(const std::string &path) {
+            _sound = path;
+        }
+
+        UI::Image &Shoot::getImage() {
+            try {
+                return (UI::Image &)getGameObject().getComponent("Image");
+            } catch (...) {
+                Debug::error("Failed to get image of button " + getGameObject().getName());
+            }
+            throw Errors::ComponentError("Failed to get image of button");
+        }
+
+        void Shoot::playSound(std::string const &string) {
+            if (_sound.size())
+                getGameObject().getEngine().getGraphicalLibManager()->getCurrentLib()->playSound(_sound);
+            else
+                getGameObject().getEngine().getGraphicalLibManager()->getCurrentLib()->playSound("Assets/SoundFX/shot1.wav");
         }
     } // KapEngine0
 } // RType
--- Assets/Components/Player/Shoot/Shoot.hpp
+++ Assets/Components/Player/Shoot/Shoot.hpp
@@ -17,19 +17,33 @@ namespace KapEngine {
         class Shoot : public Component {
         public:
             explicit Shoot(std::shared_ptr<GameObject> gameObject);
+            explicit Shoot(std::shared_ptr<GameObject> gameObject, std::string const &path, const std::string &sound);
             ~Shoot() = default;
 
             void onFixedUpdate() override;
             void animation();
 
+            void setSound(std::string const &path);
+            void setPath(std::string const &path);
+            void setPos(Tools::Vector3);
+            void setDirection(Tools::Vector2 direction);
+
         protected:
         private:
             // Variables
             Tools::Vector3 _pos;
+            Tools::Vector3 _cPos;
+            Tools::Vector2 _direction;
             std::shared_ptr<GameObject> _gameObject;
+            std::string _path = "";
+            std::string _sound = "";
+
+
 
             // Function
             void init(std::shared_ptr<GameObject> gameObject);
+            UI::Image &getImage();
+            void playSound(std::string const &string);
         };
 
     } // KapEngine

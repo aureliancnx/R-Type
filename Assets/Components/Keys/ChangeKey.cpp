44bd79f5edd74ad5909acfecfcec4799a6e1606a
benji-35 <benjamin.delvert@epitech.eu>
2022-10-08 15:07:02 +0200

    ADD : change key component

---
+++ Assets/Components/Keys/ChangeKey.cpp
@@ -0,0 +1,92 @@
+/*
+** EPITECH PROJECT, 2022
+** RType
+** File description:
+** ChangeKey
+*/
+
+#include "ChangeKey.hpp"
+#include "KeySelector.hpp"
+
+RType::ChangeKey::ChangeKey(std::shared_ptr<GameObject> go) : Component(go, "ChangeKey") {
+    addRequireComponent("KeySelector");
+
+    if (!getGameObject().hasComponent("KeySelector")) {
+        auto keySelector = std::make_shared<KeySelector>(go);
+        go->addComponent(keySelector);
+    }
+}
+
+RType::ChangeKey::~ChangeKey() {}
+
+void RType::ChangeKey::onAwake() {
+    if (_idSettings == 0 || _idInput == 0) {
+        DEBUG_ERROR("Setting GameObject and/or Input GameObject not set. Component disable.");
+        setActive(false);
+        return;
+    }
+}
+
+void RType::ChangeKey::onUpdate() {
+    if (!_startSelect)
+        return;
+    auto &keySel = getGameObject().getComponent<KeySelector>();
+    Events::Key key;
+    if (keySel.keySelected(key)) {
+        _startSelect = false;
+        PlayerPrefs::setInt(_prefName, key.get());
+        _prefName = "";
+        displaySettings();
+    }
+}
+
+void RType::ChangeKey::startSelect(std::string const& prefName) {
+    if (prefName == "") {
+        DEBUG_ERROR("You can't select a key without set a name to PlayerPrefs");
+        return;
+    }
+    _prefName = prefName;
+    auto &keySel = getGameObject().getComponent<KeySelector>();
+    keySel.selectKey();
+    displayInputs();
+}
+
+void RType::ChangeKey::displayInputs() {
+    std::shared_ptr<GameObject> go;
+    std::shared_ptr<GameObject> goS;
+    try {
+        go = getGameObject().getScene().getObject(_idInput);
+    } catch(...) {
+        DEBUG_ERROR("Failed to get GameObject for selected inputs");
+        return;
+    }
+
+    try {
+        goS = getGameObject().getScene().getObject(_idSettings);
+    } catch(...) {
+        DEBUG_ERROR("Failed to get GameObject for selected settings");
+        return;
+    }
+    go->setActive(true);
+    goS->setActive(false);
+}
+
+void RType::ChangeKey::displaySettings() {
+        std::shared_ptr<GameObject> go;
+    std::shared_ptr<GameObject> goS;
+    try {
+        go = getGameObject().getScene().getObject(_idInput);
+    } catch(...) {
+        DEBUG_ERROR("Failed to get GameObject for selected inputs");
+        return;
+    }
+
+    try {
+        goS = getGameObject().getScene().getObject(_idSettings);
+    } catch(...) {
+        DEBUG_ERROR("Failed to get GameObject for selected settings");
+        return;
+    }
+    go->setActive(false);
+    goS->setActive(true);
+}
---
+++ Assets/Components/Keys/ChangeKey.hpp
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2022
+** RType
+** File description:
+** ChangeKey
+*/
+
+#ifndef CHANGEKEY_HPP_
+#define CHANGEKEY_HPP_
+
+#include "KapEngine.hpp"
+#include "KapEngineDebug.hpp"
+#include "KapEngineEvents.hpp"
+
+using namespace KapEngine;
+
+namespace RType {
+
+    class ChangeKey : public Component {
+        public:
+            ChangeKey(std::shared_ptr<GameObject> go);
+            ~ChangeKey();
+
+            void onAwake() override;
+            void onUpdate() override;
+
+            void setCanvasSettings(std::size_t id);
+            void setCanvasInputSentence(std::size_t id);
+
+            void startSelect(std::string const& prefName);
+
+        protected:
+        private:
+            std::size_t _idSettings = 0;
+            std::size_t _idInput = 0;
+            bool _startSelect = false;
+            std::string _prefName = "";
+
+            void displayInputs();
+            void displaySettings();
+    };
+
+}
+
+#endif /* !CHANGEKEY_HPP_ */

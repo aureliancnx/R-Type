6ed44802628c517557a56a1068571caace18b114
Dosser <paul.dosser@epitech.eu>
2022-10-15 22:28:54 +0200

    ADD: parser header

---
+++ Packages/Parser/Map/Map.cpp
@@ -0,0 +1,80 @@
+/*
+** EPITECH PROJECT, 2022
+** RType
+** File description:
+** Map.cpp
+*/
+
+#include "Map.hpp"
+
+namespace RType {
+    namespace Parser {
+        Map::Map(const std::string &path) {
+            openMap(path);
+            parse();
+            closeMap();
+        }
+
+        Map::~Map() = default;
+
+        void Map::parse() {
+            std::string line;
+
+            while (std::getline(map, line)) {
+                if (line[0] == '#')
+                    parseHeader(line);
+                else if (line.empty())
+                    continue;
+                else
+                    parseSummons();
+            }
+        }
+
+        void Map::parseHeader(const std::string &line) {
+            std::string headerElement[6] = {"Name", "Authors", "Date", "Difficulty", "Description", "Background"};
+
+            for (auto elem: headerElement) {
+                if (line.find(elem) != std::string::npos) {
+                    if (elem == "Name")
+                        _name = line.substr(line.find(elem) + elem.size() + 2);
+                    else if (elem == "Authors")
+                        _author.push_back(line.substr(line.find(elem) + elem.size() + 2));
+                    else if (elem == "Date")
+                        _date = line.substr(line.find(elem) + elem.size() + 2);
+                    else if (elem == "Difficulty")
+                        _difficulty = std::stoi(line.substr(line.find(elem) + elem.size() + 2));
+                    else if (elem == "Description")
+                        _description = line.substr(line.find(elem) + elem.size() + 2);
+                    else if (elem == "Background")
+                        _background = line.substr(line.find(elem) + elem.size() + 2);
+                }
+            }
+        }
+
+        void Map::openMap(const std::string &path) {
+            map.open(path);
+            if (!map.is_open()) {
+                std::cerr << "Error: Can't open map file" << std::endl;
+                closeMap();
+                exit(84);
+            }
+        }
+
+        void Map::closeMap() {
+            map.close();
+        }
+
+        void Map::display() {
+            std::cout << "Name: " << _name << std::endl;
+            std::cout << "Authors: ";
+            for (auto author: _author)
+                std::cout << author << " ";
+            std::cout << std::endl;
+            std::cout << "Date: " << _date << std::endl;
+            std::cout << "Difficulty: " << 0 << std::endl;
+            std::cout << "Description: " << _description << std::endl;
+            std::cout << "Background: " << _background << std::endl;
+        }
+
+    } // RType
+} // Parser

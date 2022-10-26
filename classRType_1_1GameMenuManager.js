0340860d0e17aa874e5cd588f18c76f00332b055
benji-35 <benji-35@users.noreply.github.com>
2022-10-26 08:18:23 +0000

    deploy: 2e4a564b6fa96798df78639b233ea1bc2084cb5f

--- classRType_1_1GameMenuManager.js
+++ classRType_1_1GameMenuManager.js
@@ -1,6 +1,6 @@
 var classRType_1_1GameMenuManager =
 [
-    [ "GameMenuManager", "classRType_1_1GameMenuManager.html#a5fd2861515cb3de66b2352bbc6264ec2", null ],
+    [ "GameMenuManager", "classRType_1_1GameMenuManager.html#a1c21965c04d9a3662e9fc39c5280f89a", null ],
     [ "~GameMenuManager", "classRType_1_1GameMenuManager.html#a20441db59e76dfc20d7f57c87fcedc4b", null ],
     [ "displayBonusMenu", "classRType_1_1GameMenuManager.html#a5361f96eff17e68b8ccdd65cd3d30867", null ],
     [ "displayMainMenu", "classRType_1_1GameMenuManager.html#aa34dff9b5733314cd89492dad8e3dc38", null ],

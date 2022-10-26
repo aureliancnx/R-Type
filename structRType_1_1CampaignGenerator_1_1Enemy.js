4b166644b7607d06def3e96d5ec8d1b294b5248b
Ewen02 <Ewen02@users.noreply.github.com>
2022-10-26 13:51:05 +0000

    deploy: 36012b047e4ea0b266db3ea8a972d62c0ce85d9c

--- structRType_1_1CampaignGenerator_1_1Enemy.js
+++ structRType_1_1CampaignGenerator_1_1Enemy.js
@@ -1,7 +1,8 @@
 var structRType_1_1CampaignGenerator_1_1Enemy =
 [
-    [ "Enemy", "structRType_1_1CampaignGenerator_1_1Enemy.html#a5b087c4309e078cfac65d2c07e8d8db1", null ],
+    [ "Enemy", "structRType_1_1CampaignGenerator_1_1Enemy.html#ab89ac86e15836e79d2041cd5bfcbfb86", null ],
     [ "_mobName", "structRType_1_1CampaignGenerator_1_1Enemy.html#a023b471ff52af10c2a7177da08bf09c5", null ],
+    [ "_positionX", "structRType_1_1CampaignGenerator_1_1Enemy.html#a0da71a89ae0c068c369b0a1d4cb8cc8f", null ],
     [ "_positionY", "structRType_1_1CampaignGenerator_1_1Enemy.html#aa1cde07efbddd1decea43b83b948b5f8", null ],
     [ "_time", "structRType_1_1CampaignGenerator_1_1Enemy.html#abd9f79b03baac60d2c48d6a5c2e91c69", null ]
 ];

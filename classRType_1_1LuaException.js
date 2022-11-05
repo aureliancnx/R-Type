a0281eef3bc3b7f133e4ad802226cd429f1f3604
Chaika9 <Chaika9@users.noreply.github.com>
2022-11-05 12:05:38 +0000

    deploy: 3dbd6894096b559134e413573ddff2b4d103a859

--- classRType_1_1LuaException.js
+++ classRType_1_1LuaException.js
@@ -2,5 +2,5 @@ var classRType_1_1LuaException =
 [
     [ "LuaException", "classRType_1_1LuaException.html#a70ec586fa22c693d76a1e9a651b86a71", null ],
     [ "~LuaException", "classRType_1_1LuaException.html#a03f27f6118be565bae5b14927daafd71", null ],
-    [ "what", "classRType_1_1LuaException.html#a8c4edec3c7d79665b5d2878ab9f85204", null ]
+    [ "what", "classRType_1_1LuaException.html#a066f4d7fb4425469ef1e879244101776", null ]
 ];

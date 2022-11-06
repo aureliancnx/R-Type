00b2dfc0722d38b3c087e89e1da2425f80dc4d54
meredith.labejof <meredith.labejof@epitech.eu>
2022-11-06 14:10:29 +0100

    [ADD] implement the second bouboule enemy movement

--- Assets/Components/Animations/HowToPlayAnimation.cpp
+++ Assets/Components/Animations/HowToPlayAnimation.cpp
@@ -17,41 +17,68 @@ namespace RType {
         setRect(name, gameObject->getComponent<UI::Image>().getRectangle());
         // _nbAnimation.insert(std::make_pair<std::string, int>("Ship", 5));
         // _nbAnimation.insert(std::make_pair<std::string, int>("Bullet", 2));
-        // _nbAnimation.insert(std::make_pair<std::string, int>("bubulle", 12));
+        // _nbAnimation.insert(std::make_pair<std::string, int>("bouboule", 12));
         // _nbAnimation.insert(std::make_pair<std::string, int>("Explosion", 6));
         // setTiming(3);
     }
 
     void HowToPlayAnimation::init(std::shared_ptr<GameObject> gameObject) { addRequireComponent("Image"); }
 
-    void HowToPlayAnimation::moveEnemy(std::string name)
-    {
-        auto& transform = getImage(name).getTransform();
+    void HowToPlayAnimation::moveBouboule2() {
+        auto& transform = getImage("Bouboule2").getTransform();
         auto pos_Y = transform.getWorldPosition().getY();
         auto pos_X  = transform.getWorldPosition().getX();
 
-        if (_bubulleInvert) {
-            if (pos_X > 500 && pos_Y > 200)
-                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(-5.0f, -5.0f, 0));
-            else
+        if (_bouboule2Invert) {
+            // KAP_DEBUG_WARNING("X : " + std::to_string(pos_X) + " Y : " + std::to_string(pos_Y));
+            if (pos_X >= 490 && pos_Y > 200) {
                 transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(5.0f, -5.0f, 0));
+            } else {
+                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(-5.0f, -5.0f, 0));
+            }
         } else {
-            if (pos_X > 500 && pos_Y < 200)
-                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(-5.0f, 5.0f, 0));
-            else
-                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(5.0f, 5.0f, 0));                
+            if (pos_X >= 490 && pos_Y < 200) {
+                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(5.0f, 5.0f, 0));
+            } else {
+                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(-5.0f, 5.0f, 0)); 
+            }              
         }
 
         if (pos_Y > 350) {
-            _bubulleInvert = true;
+            _bouboule2Invert = true;
         } else if (pos_Y < 50) {
-            _bubulleInvert = false;
+            _bouboule2Invert = false;
         }
     }
 
-    void HowToPlayAnimation::moveShip()
-    {
-        auto& transform = getImage("ship").getTransform();
+    void HowToPlayAnimation::moveBouboule() {
+        auto& transform = getImage("Bouboule").getTransform();
+        auto pos_Y = transform.getWorldPosition().getY();
+        auto pos_X  = transform.getWorldPosition().getX();
+
+        if (_boubouleInvert) {
+            if (pos_X > 500 && pos_Y > 200) {
+                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(-5.0f, -5.0f, 0));
+            } else {
+                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(5.0f, -5.0f, 0));
+            }
+        } else {
+            if (pos_X > 500 && pos_Y < 200) {
+                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(-5.0f, 5.0f, 0));
+            } else {
+                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(5.0f, 5.0f, 0)); 
+            }              
+        }
+
+        if (pos_Y > 350) {
+            _boubouleInvert = true;
+        } else if (pos_Y < 50) {
+            _boubouleInvert = false;
+        }
+    }
+
+    void HowToPlayAnimation::moveShip() {
+        auto& transform = getImage("Ship").getTransform();
 
         if (_shipInvert) {
             transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(0, -5.0f, 0));
@@ -67,15 +94,16 @@ namespace RType {
     }
 
     void HowToPlayAnimation::onFixedUpdate() {
-        moveEnemy("bubulle");
-        // moveEnemy("bubulle_2");
+        moveBouboule();
+        moveBouboule2();
         moveShip();
     }
 
     void HowToPlayAnimation::onResetAnim() {
-        getImage("ship").setRectangle(getRect("ship"));
+        getImage("Ship").setRectangle(getRect("Ship"));
         // getImage("bullet").setRectangle({0, 0, 0, 0});
-        getImage("bubulle").setRectangle(getRect("bubulle"));
+        getImage("Bouboule").setRectangle(getRect("Bouboule"));
+        getImage("Bouboule2").setRectangle(getRect("Bouboule2"));
         // getImage("explosion").setRectangle({0, 0, 0, 0});
     }
 

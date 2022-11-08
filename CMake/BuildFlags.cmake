0b583fe5c8d1eb36c74b8da1901072b25da65c26
aureliancnx <aurelien.marcel@epitech.eu>
2022-11-08 20:33:22 +0100

    chg: remove ambiguous reversed operator warning on Clang

--- CMake/BuildFlags.cmake
+++ CMake/BuildFlags.cmake
@@ -9,4 +9,10 @@ endif()
 IF (NOT WIN32)
     target_compile_options(RType PRIVATE -O3)
 ENDIF()
+
+# MacOS dirty patch for ambiguous reversed operator warning with Clang
+if (APPLE)
+    target_compile_options(RType PRIVATE -Wno-ambiguous-reversed-operator)
+ENDIF()
+
 target_compile_options(RType PRIVATE -pthread)

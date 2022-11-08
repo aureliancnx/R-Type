d94aa28ab7fdefd211ce45ee0151e4df8f0b1406
meredith.labejof <meredith.labejof@epitech.eu>
2022-11-08 22:44:54 +0100

    [ADD] condition to active bullet display

--- Assets/Components/Animations/HowToPlayAnimation.cpp
+++ Assets/Components/Animations/HowToPlayAnimation.cpp
@@ -13,77 +13,116 @@ namespace RType {
 
     HowToPlayAnimation::HowToPlayAnimation(std::string name, std::shared_ptr<KapEngine::GameObject> gameObject) : Animation(gameObject) {
         init(gameObject);
-        // _nbAnimation.insert(std::make_pair<std::string, int>("Ship", 5));
-        // _nbAnimation.insert(std::make_pair<std::string, int>("Bullet", 2));
-        // _nbAnimation.insert(std::make_pair<std::string, int>("bouboule", 12));
-        // _nbAnimation.insert(std::make_pair<std::string, int>("Explosion", 6));
-        // setTiming(3);
     }
 
     void HowToPlayAnimation::init(std::shared_ptr<GameObject> gameObject) { addRequireComponent("Image"); }
 
+    void HowToPlayAnimation::onFixedUpdate() {
+        moveBouboule();
+        moveBouboule2();
+        moveShip();
+        moveBullet();
+    }
+
+    void HowToPlayAnimation::onResetAnim() {
+        // getImage("Ship").setRectangle(getRect("Ship"));
+        // getImage("Bullet").setRectangle(getRect("Bullet"));
+        // getImage("Bouboule").setRectangle(getRect("Bouboule"));
+        // getImage("Bouboule2").setRectangle(getRect("Bouboule2"));
+        // getImage("Explosion").setRectangle(getRect("Explosion"));
+
+        // auto& transform = getImage("Ship").getTransform();
+        // transform.setPosition(getResetPosition("Ship"));
+
+        // auto& transform = getImage("Bullet").getTransform();
+        // transform.setPosition(getResetPosition("Bullet"));
+
+        // auto& transform = getImage("Bouboule").getTransform();
+        // transform.setPosition(getResetPosition("Bouboule"));
+
+        // auto& transform = getImage("Bouboule2").getTransform();
+        // transform.setPosition(getResetPosition("Bouboule2"));
+
+        // auto& transform = getImage("Explosion").getTransform();
+        // transform.setPosition(getResetPosition("Explosion"));
+    }
+
     void HowToPlayAnimation::moveBouboule2() {
         auto& transform = getImage("Bouboule2").getTransform();
+        // KAP_DEBUG_WARNING(" BOUBOULE 2 X : " + std::to_string(transform.getWorldPosition().getX()) + " Y : " + std::to_string(transform.getWorldPosition().getY()));
         auto pos_Y = transform.getWorldPosition().getY();
         auto pos_X  = transform.getWorldPosition().getX();
 
         if (_bouboule2Invert) {
             if (pos_X >= 490 && pos_Y > 200) {
-                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(5.0f, -5.0f, 0));
+                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(4.0f, -4.0f, 0));
             } else {
-                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(-5.0f, -5.0f, 0));
+                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(-4.0f, -4.0f, 0));
             }
         } else {
             if (pos_X >= 490 && pos_Y < 200) {
-                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(5.0f, 5.0f, 0));
+                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(4.0f, 4.0f, 0));
             } else {
-                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(-5.0f, 5.0f, 0)); 
+                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(-4.0f, 4.0f, 0)); 
             }              
         }
 
-        if (pos_Y > 350) {
+        if (pos_Y > 300) {
             _bouboule2Invert = true;
-        } else if (pos_Y < 50) {
+            _canShoot2 = true;
+        } else if (pos_Y < 70) {
             _bouboule2Invert = false;
         }
     }
 
     void HowToPlayAnimation::moveBouboule() {
         auto& transform = getImage("Bouboule").getTransform();
+        // KAP_DEBUG_WARNING(" BOUBOULE X : " + std::to_string(transform.getWorldPosition().getX()) + " Y : " + std::to_string(transform.getWorldPosition().getY()));
         auto pos_Y = transform.getWorldPosition().getY();
         auto pos_X  = transform.getWorldPosition().getX();
 
         if (_boubouleInvert) {
             if (pos_X > 500 && pos_Y > 200) {
-                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(-5.0f, -5.0f, 0));
+                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(-4.0f, -4.0f, 0));
             } else {
-                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(5.0f, -5.0f, 0));
+                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(4.0f, -4.0f, 0));
             }
         } else {
             if (pos_X > 500 && pos_Y < 200) {
-                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(-5.0f, 5.0f, 0));
+                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(-4.0f, 4.0f, 0));
             } else {
-                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(5.0f, 5.0f, 0)); 
+                transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(4.0f, 4.0f, 0)); 
             }              
         }
 
-        if (pos_Y > 350) {
+        if (pos_Y == 266.0f) {
+            // KAP_DEBUG_WARNING("GOT YOU");
+        }
+
+        if (pos_Y > 300) {
             _boubouleInvert = true;
-        } else if (pos_Y < 50) {
+            _canShoot = true;
+        } else if (pos_Y < 70) {
             _boubouleInvert = false;
         }
     }
 
-    void HowToPlayAnimation::moveShip() {
+    void HowToPlayAnimation::moveShip() {    
         auto& transform = getImage("Ship").getTransform();
+        // KAP_DEBUG_WARNING(" SHIP X : " + std::to_string(transform.getWorldPosition().getX()) + " Y : " + std::to_string(transform.getWorldPosition().getY()));
 
         if (_shipInvert) {
-            transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(0, -5.0f, 0));
+            transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(0, -4.0f, 0));
         } else {
-            transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(0, 5.0f, 0));
+            transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(0, 4.0f, 0));
         }
 
+        // if (transform.getWorldPosition().getY() == 280 || transform.getWorldPosition().getY() == 100) {
+            // KAP_DEBUG_WARNING("hello");
+        //     _nbInvert++;
+        // }
         if (transform.getWorldPosition().getY() > 300) {
+            // KAP_DEBUG_WARNING("X : " + std::to_string(transform.getWorldPosition().getX()) + " Y : " + std::to_string(transform.getWorldPosition().getY()));
             _shipInvert = true;
             _allGameObject.at("Ship")->getComponent<KapEngine::Animator>().setTrigger("DownToIdle");
             _allGameObject.at("Ship")->getComponent<KapEngine::Animator>().setTrigger("IdleToUp");
@@ -96,8 +135,14 @@ namespace RType {
 
     void HowToPlayAnimation::moveBullet() {
         auto& transform = getImage("Bullet").getTransform();
+        if (getGameObject("Bullet")->isActive() == false && getImage("Ship").getTransform().getLocalPosition().getY() == 266) {
+            getGameObject("Bullet")->setActive(true);
+            transform.setPosition(getResetPosition("Bullet"));
+        }
+        if (getGameObject("Bullet")->isActive() == false)
+            return;
 
-        transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(5.0f, 0, 0));
+        transform.setPosition(transform.getLocalPosition() + KapEngine::Tools::Vector3(4.0f, 0, 0));
 
         if (transform.getWorldPosition().getX() < 500) {
             _allGameObject.at("Bullet")->getComponent<KapEngine::Animator>().setTrigger("BigBulletToLittleBullet");
@@ -108,33 +153,8 @@ namespace RType {
         }
 
         if (transform.getWorldPosition().getX() > 600) {
-            _bulletReset = true;
+            getGameObject("Bullet")->setActive(false);
         }
-    }
-
-    void HowToPlayAnimation::onFixedUpdate() {
-        // KAP_DEBUG_WARNING("X : " + std::to_string(getImage("Bullet").getTransform().getWorldPosition().getX()) + " Y : " + std::to_string(getImage("Bullet").getTransform().getWorldPosition().getY()));
-        moveBouboule();
-        moveBouboule2();
-        moveShip();
-        //ajouter un temps avant d'afficher le bullet
-        if (!_bulletReset) {
-            moveBullet();
-        } else {
-            onResetAnim();
-        }
-    }
-
-    void HowToPlayAnimation::onResetAnim() {
-        // getImage("Ship").setRectangle(getRect("Ship"));
-        // getImage("Bullet").setRectangle(getRect("Bullet"));
-        // getImage("Bouboule").setRectangle(getRect("Bouboule"));
-        // getImage("Bouboule2").setRectangle(getRect("Bouboule2"));
-        // getImage("explosion").setRectangle({0, 0, 0, 0});
-
-        auto& transform = getImage("Bullet").getTransform();
-        transform.setPosition(getResetPosition("Bullet"));
-        _bulletReset = false;
     }
 
     void HowToPlayAnimation::setNbAnimations(std::string name, int nb) { _allGameObject.at(name)->getComponent<SpriteAnimation>().setNbAnimations(nb); }
@@ -154,7 +174,7 @@ namespace RType {
         else
             _allGoResetPosition.at(name) = position; 
     }
-    
+
     KapEngine::Tools::Vector3 HowToPlayAnimation::getResetPosition(std::string name) {
         try {
             return (_allGoResetPosition.at(name)); 
@@ -181,6 +201,13 @@ namespace RType {
             return _allGameObject.at(name)->getComponent<SpriteAnimation>();
         } catch (...) { Debug::error("Failed to get SpriteAnimation of  " + _allGameObject.at(name)->getName()); }
         throw Errors::ComponentError("Failed to get SpriteAnimation of ");
+    }
+
+    std::shared_ptr<KapEngine::GameObject>& HowToPlayAnimation::getGameObject(std::string name) {
+        try {
+            return _allGameObject.at(name);
+        } catch (...) { Debug::error("Failed to get gameObject of  " + _allGameObject.at(name)->getName()); }
+        throw Errors::ComponentError("Failed to get gameObject of ");
     }
 
 } // namespace RType

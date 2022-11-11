8beed2a43ae0359ce36b16c8d914258b7cdc9ea9
aureliancnx <aurelien.marcel@epitech.eu>
2022-11-11 13:01:46 +0100

    add: save netstatviewer texts in local var

--- Assets/Components/NetStatViewer.hpp
+++ Assets/Components/NetStatViewer.hpp
@@ -12,7 +12,11 @@ namespace RType {
       private:
         long long lastRefreshTime = 0;
         std::shared_ptr<KapMirror::Experimental::NetworkStatistics> statObject;
+
         std::shared_ptr<KapEngine::GameObject> textReceivedPackets;
+        std::shared_ptr<KapEngine::GameObject> textSentPackets;
+        std::shared_ptr<KapEngine::GameObject> textReceivedPacketsPerSec;
+        std::shared_ptr<KapEngine::GameObject> textSentPacketsPerSec;
 
       public:
         explicit NetStatViewer(std::shared_ptr<KapEngine::GameObject> _gameObject, std::shared_ptr<KapMirror::Experimental::NetworkStatistics> _statObject);

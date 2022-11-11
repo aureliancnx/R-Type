4496cd198e4c1d03beb47b597570a1ee4c57f516
Chaika9 <bestdevelop22@gmail.com>
2022-11-11 18:53:02 +0100

    Fix TestMap

--- Maps/TestMap.lua
+++ Maps/TestMap.lua
@@ -3,7 +3,7 @@
 Map.SetName("TestMap")
 Map.SetAuthor("Chaika9")
 Map.SetDescription("This is a test map for RType")
-Map.SetBannerPath("Campaign test")
+Map.SetBannerPath("Assets/Textures/Level Campaign/TestMap.png")
 
 -- Register new enemies
 
@@ -29,9 +29,8 @@ redShip2.scale = Vector2.new(79, 35)
 
 -- Controller of the enemies
 function BasicController(posX, posY, time)
-    if (time % 1000) then
-        Debug.Log("Bullet")
-        Map.InstanciatePrefab("Bullet", posX, posY)
+    if (Math.Mod(time, 50) == 0) then
+        Map.InstantiatePrefab("Bullet", posX, posY)
     end
     return posX - 0.5, posY
 end
@@ -50,10 +49,6 @@ function OnEnemyUpdate(enemyName, posX, posY, time)
 end
 
 -- Spawn enemies
-Map.SpawnEnemy("Bouboule2", 1, 100, 100, 10)
-
---for i = 1, 3 do
---    Map.SpawnEnemy("Bouboule2", 1, 100 + (60 * i), 100, 10)
---end
---
---Map.SpawnEnemy("RedShip2", 1, 400, 100, 10)
+for i = 1, 3 do
+    Map.SpawnEnemy("Bouboule2", 1, 100 + (60 * i), 100, 10)
+end

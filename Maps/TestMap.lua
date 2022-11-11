eea7452a4d6a28e8bca65dccc921099d0abb999d
Chaika9 <bestdevelop22@gmail.com>
2022-11-11 19:20:32 +0100

    Edit TestMap

--- Maps/TestMap.lua
+++ Maps/TestMap.lua
@@ -19,13 +19,28 @@ bouboule.name = "Bouboule"
 bouboule.pathSprite = "Assets/Textures/Enemy/enemy_1.png"
 bouboule.rectangle = Rectangle.new(0, 0, 17, 18)
 bouboule.scale = Vector2.new(51, 54)
-bouboule.animation = bouboule2Anim
+bouboule.animation = boubouleAnim
 
+-- RedShip
 local redShip = Enemy.new()
 redShip.name = "RedShip"
 redShip.pathSprite = "Assets/Textures/Enemy/enemy_4.png"
 redShip.rectangle = Rectangle.new(0, 0, 263, 116)
 redShip.scale = Vector2.new(79, 35)
+
+-- Tentacle Boss
+local tentacleBossAnim = SpriteAnimation.new()
+tentacleBossAnim.duration = 0.8
+tentacleBossAnim.loop = true
+tentacleBossAnim.rectangle = Rectangle.new(0, 0, 65.5, 66)
+tentacleBossAnim.nbFrames = 12
+
+local tentacleBoss = Enemy.new()
+tentacleBoss.name = "TentacleBoss"
+tentacleBoss.pathSprite = "Assets/Textures/Enemy/enemy_3.png"
+tentacleBoss.rectangle = Rectangle.new(0, 0, 64, 66)
+tentacleBoss.scale = Vector2.new(128, 132)
+tentacleBoss.animation = tentacleBossAnim
 
 -- Controller of the enemies
 function BasicController(posX, posY, time)
@@ -35,10 +50,21 @@ function BasicController(posX, posY, time)
     return posX - 0.5, posY
 end
 
+function BossController(posX, posY, time)
+    -- Make the boss movement
+
+    -- Shoot
+    if (Math.Mod(time, 50) == 0) then
+        Map.InstantiatePrefab("Bullet", posX, posY)
+    end
+    return posX, posY
+end
+
 -- Update enemies
 local controllers = {
     ["Bouboule"] = BasicController,
-    ["RedShip"] = BasicController
+    ["RedShip"] = BasicController,
+    ["TentacleBoss"] = BossController
 }
 
 function OnEnemyUpdate(enemyName, posX, posY, time)
@@ -54,3 +80,5 @@ for i = 1, 3 do
 end
 
 Map.SpawnEnemy("RedShip", 8, Math.Random(200, 400), 10, 50)
+
+Map.SpawnEnemy("TentacleBoss", 15, 300, 200, 100)

656183e891ef393a8284776f9e54fb8c510275f9
aureliancnx <aurelien.marcel@epitech.eu>
2022-11-11 13:01:10 +0100

    chg: init other texts - netstatviewer

--- Assets/Components/NetStatViewer.cpp
+++ Assets/Components/NetStatViewer.cpp
@@ -10,14 +10,43 @@ NetStatViewer::NetStatViewer(std::shared_ptr<KapEngine::GameObject> _gameObject,
 }
 
 void NetStatViewer::onAwake() {
+    auto canvas = KapEngine::UI::UiFactory::createCanvas(getGameObject().getScene(), "Canvas NetStatViewer");
     {
-        auto canvas = KapEngine::UI::UiFactory::createCanvas(getGameObject().getScene(), "Canvas NetStatViewer");
-
-        textReceivedPackets = KapEngine::UI::UiFactory::createText(getGameObject().getScene(), "Test test");
+        textReceivedPackets = KapEngine::UI::UiFactory::createText(getGameObject().getScene(), "Packets received");
         auto &text = textReceivedPackets->getComponent<KapEngine::UI::Text>();
         auto &transform = textReceivedPackets->getComponent<KapEngine::Transform>().getTransform();
 
-        text.setText("Coucou");
+        text.setText("Packets received: 0");
+        transform.setScale(KapEngine::Tools::Vector3(150, 35, 0));
+        transform.setPosition(KapEngine::Tools::Vector3(50, 150, 0));
+        transform.setParent(canvas);
+    }
+    {
+        textSentPackets = KapEngine::UI::UiFactory::createText(getGameObject().getScene(), "Packets Sent");
+        auto &text = textSentPackets->getComponent<KapEngine::UI::Text>();
+        auto &transform = textSentPackets->getComponent<KapEngine::Transform>().getTransform();
+
+        text.setText("Packets sent: 0");
+        transform.setScale(KapEngine::Tools::Vector3(150, 35, 0));
+        transform.setPosition(KapEngine::Tools::Vector3(50, 150, 0));
+        transform.setParent(canvas);
+    }
+    {
+        textReceivedPacketsPerSec = KapEngine::UI::UiFactory::createText(getGameObject().getScene(), "Packets received per second");
+        auto &text = textReceivedPacketsPerSec->getComponent<KapEngine::UI::Text>();
+        auto &transform = textReceivedPacketsPerSec->getComponent<KapEngine::Transform>().getTransform();
+
+        text.setText("Packet/s received: 0");
+        transform.setScale(KapEngine::Tools::Vector3(150, 35, 0));
+        transform.setPosition(KapEngine::Tools::Vector3(50, 150, 0));
+        transform.setParent(canvas);
+    }
+    {
+        textSentPacketsPerSec = KapEngine::UI::UiFactory::createText(getGameObject().getScene(), "Packets sent per second");
+        auto &text = textSentPacketsPerSec->getComponent<KapEngine::UI::Text>();
+        auto &transform = textSentPacketsPerSec->getComponent<KapEngine::Transform>().getTransform();
+
+        text.setText("Packet/s sent: 0");
         transform.setScale(KapEngine::Tools::Vector3(150, 35, 0));
         transform.setPosition(KapEngine::Tools::Vector3(50, 150, 0));
         transform.setParent(canvas);
@@ -30,8 +59,10 @@ void NetStatViewer::onFixedUpdate() {
         lastRefreshTime = KapMirror::NetworkTime::localTime();
         std::cout << "NetStatViewer: fixed Update!" << std::endl;
 
-        auto &text = textReceivedPackets->getComponent<KapEngine::UI::Text>();
-        text.setText("Ouais bien: " + std::to_string(statObject->clientIntervalReceivedPackets));
+        {
+            auto &text = textReceivedPackets->getComponent<KapEngine::UI::Text>();
+            text.setText("Packets received: " + std::to_string(statObject->clientIntervalReceivedPackets));
+        }
         // TODO
     }
 }

447f4c15375d2e7d54a35df46ed79237dff998e8
Dosser <paul.dosser@epitech.eu>
2022-11-11 17:36:18 +0100

    ADD: setup lobby manager

---
+++ Assets/Components/LobbyManager.hpp
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2022
+** RType
+** File description:
+** LobbyManager.hpp
+*/
+
+#ifndef RTYPE_LOBBYMANAGER_HPP
+#define RTYPE_LOBBYMANAGER_HPP
+
+#include "KapEngine.hpp"
+#include "KapMirror/KapMirror.hpp"
+#include "KapEngineUi.hpp"
+#include "KapUI/KapUI.hpp"
+
+namespace KapEngine {
+
+    class LobbyManager : public KapMirror::NetworkComponent {
+      public:
+        explicit LobbyManager(std::shared_ptr<KapEngine::GameObject> go);
+        ~LobbyManager();
+
+        void onStart() override;
+
+        void onStartClient() override;
+
+      private:
+        // Methods
+        void initLobby(bool local = true);
+
+        // Variables
+        std::shared_ptr<KapEngine::GameObject> mainMenu;
+        std::shared_ptr<KapEngine::GameObject> ready;
+    };
+
+} // namespace KapEngine
+
+#endif // RTYPE_LOBBYMANAGER_HPP

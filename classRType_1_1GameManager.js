3dc6eed1046e565322219e9572559a34366afe38
Chaika9 <Chaika9@users.noreply.github.com>
2022-11-12 16:50:57 +0000

    deploy: 6d1cf18d1b303e825a0ce280439ad046444f3097

--- classRType_1_1GameManager.js
+++ classRType_1_1GameManager.js
@@ -3,7 +3,7 @@ var classRType_1_1GameManager =
     [ "GameManager", "classRType_1_1GameManager.html#ae1cf7c831cb3b203ebabfaf5a4de5fb5", null ],
     [ "~GameManager", "classRType_1_1GameManager.html#a97084bc427fc6a9ce22aa089f8ac6ce4", null ],
     [ "getNetworkManager", "classRType_1_1GameManager.html#ae860dff5dbf204feee000480ae93088b", null ],
-    [ "hasDebugMode", "classRType_1_1GameManager.html#ab851caa8e21ed9624d7ac49ca0c2a426", null ],
+    [ "hasDebugMode", "classRType_1_1GameManager.html#a8bf9361c19b3ea3388d4462b08a38d7a", null ],
     [ "launchGame", "classRType_1_1GameManager.html#a60491742fd343d211117dea4fef4f280", null ],
     [ "launchServer", "classRType_1_1GameManager.html#a0cf2fed97d76428a281b6cab08ac6ce3", null ],
     [ "startCampaign", "classRType_1_1GameManager.html#a4fbbc0a25059d10afc0357d677e1583b", null ],

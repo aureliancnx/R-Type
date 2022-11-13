f5242d2dc11b6b9bdea09f6ac29d5797cc4a3e08
Ewen02 <ewen.le-quere@epitech.eu>
2022-11-13 19:49:49 +0100

    [ADD] première version d'un level, avec des ennemies qui ont des mouvement verticale

---
+++ Maps/Level1.lua
@@ -0,0 +1,108 @@
+-- Level 1
+
+Map.SetName("Level 1")
+Map.SetAuthor("Xtr2m")
+Map.SetDescription("This is a level 1 map for RType")
+Map.SetBannerPath("Assets/Textures/Level Campaign/lvl2_img.png")
+
+-- Register new enemies
+
+-- Enemy 2
+
+--[[local boubouleAnim = SpriteAnimation.new()
+boubouleAnim.duration = 0.5
+boubouleAnim.loop = true
+boubouleAnim.rectangle = Rectangle.new(0, 0, 33, 36)
+boubouleAnim.nbFrames = 12
+
+local bouboule = Enemy.new()
+bouboule.name = "Bouboule2"
+bouboule.pathSprite = "Assets/Textures/Enemy/enemy_2.png"
+bouboule.rectangle = Rectangle.new(0, 0, 33, 36)
+bouboule.scale = Vector2.new(51, 54)
+bouboule.animation = boubouleAnim]]
+
+-- Bouboule
+local boubouleAnim = SpriteAnimation.new()
+boubouleAnim.duration = 0.5
+boubouleAnim.loop = true
+boubouleAnim.rectangle = Rectangle.new(0, 0, 17, 18)
+boubouleAnim.nbFrames = 12
+
+local bouboule = Enemy.new()
+bouboule.name = "Bouboule2"
+bouboule.pathSprite = "Assets/Textures/Enemy/enemy_1.png"
+bouboule.rectangle = Rectangle.new(0, 0, 17, 18)
+bouboule.scale = Vector2.new(51, 54)
+bouboule.animation = boubouleAnim
+
+-- RedShip
+local redShip = Enemy.new()
+redShip.name = "RedShip2"
+redShip.pathSprite = "Assets/Textures/Enemy/enemy_4.png"
+redShip.rectangle = Rectangle.new(0, 0, 263, 116)
+redShip.scale = Vector2.new(79, 35)
+
+-- Tentacle Boss
+local tentacleBossAnim = SpriteAnimation.new()
+tentacleBossAnim.duration = 0.8
+tentacleBossAnim.loop = true
+tentacleBossAnim.rectangle = Rectangle.new(0, 0, 65.5, 66)
+tentacleBossAnim.nbFrames = 12
+
+local tentacleBoss = Enemy.new()
+tentacleBoss.name = "TentacleBoss2"
+tentacleBoss.pathSprite = "Assets/Textures/Enemy/enemy_3.png"
+tentacleBoss.rectangle = Rectangle.new(0, 0, 64, 66)
+tentacleBoss.scale = Vector2.new(128, 132)
+tentacleBoss.animation = tentacleBossAnim
+
+-- Controller of the enemies
+function BasicController(posX, posY, time)
+    if (Math.Mod(time, 50) == 0) then
+        Map.InstantiatePrefab("Bullet", posX, posY)
+    end
+    if (Math.Random(-10, 10) > 0) then
+        return posX - 0.5, posY - 1.5
+    else
+        return posX - 0.5, posY + 1.5
+    end
+end
+
+function BossController(posX, posY, time)
+    -- Make the boss movement
+
+    -- Shoot
+    if (Math.Mod(time, 50) == 0) then
+        Map.InstantiatePrefab("Bullet", posX, posY)
+    end
+
+    if (posY < 100) then
+        return posX, posY + 0.5
+    else
+        return posX, posY - 0.5
+    end
+end
+
+-- Update enemies
+local controllers = {
+    ["Bouboule2"] = BasicController,
+    ["RedShip2"] = BasicController,
+    ["TentacleBoss2"] = BossController
+}
+
+function OnEnemyUpdate(enemyName, posX, posY, time)
+    if controllers[enemyName] ~= nil then
+        return controllers[enemyName](posX, posY, time)
+    end
+    return posX, posY
+end
+
+-- Spawn enemies
+for i = 1, 100 do
+    Map.SpawnEnemy("Bouboule2", 4 * i, Math.Random(100, 500), 0, 5)
+end
+
+--Map.SpawnEnemy("RedShip2", 8, Math.Random(200, 400), 0, 50)
+
+Map.SpawnEnemy("TentacleBoss2", 15, 300, 200, 100)

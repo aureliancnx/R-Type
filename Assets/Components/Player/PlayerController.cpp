bcab868bb71a3fa358fdbf5c67961f0e0811ce9f
Linter <runner@fv-az504-947.a4wi22hdjusufhmrdcovmrycyb.dx.internal.cloudapp.net>
2022-11-13 17:22:51 +0000

    CHG: Apply linter

--- Assets/Components/Player/PlayerController.cpp
+++ Assets/Components/Player/PlayerController.cpp
@@ -163,8 +163,7 @@ void PlayerController::prepareShoot() {
 }
 
 void PlayerController::shoot() {
-    KapEngine::Tools::Vector3 pos = getTransform().getLocalPosition()
-            + KapEngine::Tools::Vector3(90, 15, 0);
+    KapEngine::Tools::Vector3 pos = getTransform().getLocalPosition() + KapEngine::Tools::Vector3(90, 15, 0);
     bool isMissile = false;
 
     if (clockMissile.getElapseTime().asSecond() >= 4.5f) {

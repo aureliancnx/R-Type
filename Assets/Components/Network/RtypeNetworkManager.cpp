63a53ff490f027767fea1446cd79df66dc573e33
Linter <runner@fv-az375-301.50a1pkzc4r3edj423go120ring.dx.internal.cloudapp.net>
2022-11-13 15:23:02 +0000

    CHG: Apply linter

--- Assets/Components/Network/RtypeNetworkManager.cpp
+++ Assets/Components/Network/RtypeNetworkManager.cpp
@@ -113,23 +113,23 @@ void RtypeNetworkManager::onPlayerStartGameMessage(const std::shared_ptr<KapMirr
                                                    StartGameMessage& message) {
     KAP_DEBUG_LOG("onPlayerStartGameMessage: Start game");
 
-//    try {
-//        auto go = getScene().findFirstGameObject("LobbyManger");
-//        if (go) {
-//            go->setActive(false);
-//        }
-//    } catch (...) {
-//        KAP_DEBUG_ERROR("onPlayerStartGameMessage: LobbyManager not found");
-//    }
-//
-//    try {
-//        auto go = getScene().findFirstGameObject("MenuManager");
-//        if (go) {
-//            go->setActive(true);
-//        }
-//    } catch (...) {
-//        KAP_DEBUG_ERROR("onPlayerStartGameMessage: MenuManager not found");
-//    }
+    //    try {
+    //        auto go = getScene().findFirstGameObject("LobbyManger");
+    //        if (go) {
+    //            go->setActive(false);
+    //        }
+    //    } catch (...) {
+    //        KAP_DEBUG_ERROR("onPlayerStartGameMessage: LobbyManager not found");
+    //    }
+    //
+    //    try {
+    //        auto go = getScene().findFirstGameObject("MenuManager");
+    //        if (go) {
+    //            go->setActive(true);
+    //        }
+    //    } catch (...) {
+    //        KAP_DEBUG_ERROR("onPlayerStartGameMessage: MenuManager not found");
+    //    }
 }
 
 void RtypeNetworkManager::onClientPlayerPingRequest(const std::shared_ptr<KapMirror::NetworkConnectionToServer>& connection,
